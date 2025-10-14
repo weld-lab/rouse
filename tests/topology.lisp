e44ba5ca787f34e359c57ffdd7548ecd5873ddac
atomicwelding <ctfweld@gmail.com>
2025-10-14 15:33:11 +0200

    test for center of mass

--- tests/topology.lisp
+++ tests/topology.lisp
@@ -8,3 +8,15 @@
 		(:x 0.0 :y 1.0 :z 0.0 :vx 1.0))))
     (is (= (length (top::chain-beads chain))
 	   2))))
+
+
+(test chain-center-of-mass
+  (let ((chain (top:make-chain
+		(:x 1.0 :mass 3.0)
+		(:x 0.0 :mass 2.0))))
+    (is (equal '(0.6 0.0 0.0) (top:center-of-mass chain))))
+
+  (let ((chain (top:make-chain
+		(:x 1.0 :mass -1.0)
+		(:x 0.0 :mass  1.0))))
+    (is (equal '(0.0 0.0 0.0) (top:center-of-mass chain)))))
