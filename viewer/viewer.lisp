ddf1b19d581b02c46aa88bda43ba6c39fe2259f4
atomicwelding <ctfweld@gmail.com>
2025-10-14 12:48:03 +0200

    correct method call

--- viewer/viewer.lisp
+++ viewer/viewer.lisp
@@ -3,7 +3,7 @@
 
 (defmethod view ((sim sim:simulation)
 		 &key (width 400) (height 400) (mode :global-view))
-  (cond ((eq mode :global-view) (global-view-mode))
-	((eq mode :ortho-view) (ortho-view-mode))
+  (cond ((eq mode :global-view) (global-view-mode sim width height))
+	((eq mode :ortho-view) (ortho-view-mode sim width height))
 	(t (error "This view mode doesn't exist
 Use :global-view or :ortho-view"))))
