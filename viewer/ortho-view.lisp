30d2a056f06e041637573a7badbea51cbd0453a8
atomicwelding <ctfweld@gmail.com>
2025-10-15 11:01:40 +0200

    refactor

--- viewer/ortho-view.lisp
+++ viewer/ortho-view.lisp
@@ -1,41 +1,68 @@
 (in-package #:rouse.viewer)
 
+
+
+(defclass ortho-state ()
+  ((ortho-state-i :accessor ortho-state-i
+                  :initform 0)
+   (ortho-state-faces :accessor ortho-state-faces
+                      :initform *params-ortho-faces*)))
+
+
+
+(defmethod ortho-get-face ((os ortho-state))
+  (aref (ortho-state-faces os) (ortho-state-i os)))
+
+
+
+(defmethod ortho-go-next-face ((os ortho-state))
+  (incf (ortho-state-i os))
+  (when (>= (ortho-state-i os) (length (ortho-state-faces os)))
+    (setf (ortho-state-i os) 0))
+  (ortho-get-face os))
+
+
+
+(defmethod ortho-input ((sim sim:simulation) (os ortho-state)
+			camera)
+  (when (is-key-pressed :key-space)
+    (let ((new-face (ortho-go-next-face os)))
+      (setf (camera3d-position camera) (first new-face))
+      (setf (camera3d-up camera) (second new-face))
+      (format t "~&Switched to face ~A~%" (third new-face)))))
+
+
+
+(defmethod ortho-draw ((sim sim:simulation) (os ortho-state)
+		       camera)
+  (let* ((state (sim:current-state sim))
+         (chain (top:remove-center-of-mass (sim:state-chain state))))
+    (with-drawing
+      (clear-background :black)
+      (with-mode-3d (camera)
+        (render-chain chain)))))
+
+
+
+(defmethod ortho-initialize-camera ((sim sim:simulation) (os ortho-state))
+  (let ((face (ortho-get-face os)))
+    (make-camera3d
+     :position (first face)
+     :target (vec 0.0 0.0 0.0)
+     :up (second face)
+     :fovy 10.0
+     :projection :camera-orthographic)))
+
+
+
 (defmethod ortho-view-mode ((sim sim:simulation))
   (set-trace-log-level *params-log-level*)
-
-  (let* ((faces (vector
-                 (list (vec  5.0  0.0  0.0) (vec 0.0 0.0 1.0) "+X")
-                 (list (vec -5.0  0.0  0.0) (vec 0.0 0.0 1.0) "-X")
-                 (list (vec  0.0  5.0  0.0) (vec 0.0 0.0 1.0) "+Y")
-                 (list (vec  0.0 -5.0  0.0) (vec 0.0 0.0 1.0) "-Y")
-                 (list (vec  0.0  0.0  5.0) (vec 0.0 1.0 0.0) "+Z")
-                 (list (vec  0.0  0.0 -5.0) (vec 0.0 1.0 0.0) "-Z")))
-         (i 0))
-
-    (let ((camera (make-camera3d
-                   :position (first (aref faces i))
-                   :target (vec 0.0 0.0 0.0)
-                   :up (second (aref faces i))
-                   :fovy 10.0
-                   :projection :camera-orthographic)))
-
-      (destructuring-bind (width height title) *params-window-params*
-        (with-window (width height title)
-          (set-target-fps *params-target-fps*)
-
-          (loop until (window-should-close)
-                do
-
-		   ;; input
-                   (when (is-key-pressed :key-space)
-                     (setf i (mod (1+ i) (length faces)))
-                     (setf (camera3d-position camera) (first (aref faces i)))
-                     (setf (camera3d-up camera) (second (aref faces i)))
-                     (format t "~&Switched to face ~A~%" (third (aref faces i))))
-
-		   ;; draw
-                   (let ((state (sim:current-state sim)))
-                     (with-drawing
-                       (clear-background :black)
-                       (with-mode-3d (camera)
-                         (render-chain (sim:state-chain state)))))))))))
+  (let* ((os (make-instance 'ortho-state))
+         (camera (ortho-initialize-camera sim os)))
+    (destructuring-bind (width height title) *params-window-params*
+      (with-window (width height title)
+        (set-target-fps *params-target-fps*)
+        (loop until (window-should-close)
+              do
+		 (ortho-input sim os camera)
+		 (ortho-draw  sim os camera))))))
