172275cf10d7e9ab8d856f4e599bf66d2f9c72bf
atomicwelding <ctfweld@gmail.com>
2025-10-15 08:58:38 +0200

    draft view

--- viewer/ortho-view.lisp
+++ viewer/ortho-view.lisp
@@ -1,20 +1,41 @@
 (in-package #:rouse.viewer)
 
-
 (defmethod ortho-view-mode ((sim sim:simulation))
   (set-trace-log-level *params-log-level*)
-  (let ((camera (make-camera3d :position (vec -5.0 0.0 0.0)
-			       :target (vec 0.0 0.0 0.0)
-			       :up (vec 0.0 0.0 1.0)
-			       :fovy 10.0
-			       :projection :camera-orthographic)))
 
-    (destructuring-bind (width height title) *params-window-params*
-      (with-window (width height title)
-	(set-target-fps *params-target-fps*)
-	(loop until (window-should-close)
-	      do (let ((state (sim:current-state sim)))
-		   (with-drawing
-		     (clear-background :black)
-		     (with-mode-3d (camera)
-		       (render-chain (sim:state-chain state))))))))))
+  (let* ((faces (vector
+                 (list (vec  5.0  0.0  0.0) (vec 0.0 0.0 1.0) "+X")
+                 (list (vec -5.0  0.0  0.0) (vec 0.0 0.0 1.0) "-X")
+                 (list (vec  0.0  5.0  0.0) (vec 0.0 0.0 1.0) "+Y")
+                 (list (vec  0.0 -5.0  0.0) (vec 0.0 0.0 1.0) "-Y")
+                 (list (vec  0.0  0.0  5.0) (vec 0.0 1.0 0.0) "+Z")
+                 (list (vec  0.0  0.0 -5.0) (vec 0.0 1.0 0.0) "-Z")))
+         (i 0))
+
+    (let ((camera (make-camera3d
+                   :position (first (aref faces i))
+                   :target (vec 0.0 0.0 0.0)
+                   :up (second (aref faces i))
+                   :fovy 10.0
+                   :projection :camera-orthographic)))
+
+      (destructuring-bind (width height title) *params-window-params*
+        (with-window (width height title)
+          (set-target-fps *params-target-fps*)
+
+          (loop until (window-should-close)
+                do
+
+		   ;; input
+                   (when (is-key-pressed :key-space)
+                     (setf i (mod (1+ i) (length faces)))
+                     (setf (camera3d-position camera) (first (aref faces i)))
+                     (setf (camera3d-up camera) (second (aref faces i)))
+                     (format t "~&Switched to face ~A~%" (third (aref faces i))))
+
+		   ;; draw
+                   (let ((state (sim:current-state sim)))
+                     (with-drawing
+                       (clear-background :black)
+                       (with-mode-3d (camera)
+                         (render-chain (sim:state-chain state)))))))))))
--- viewer/rendering.lisp
+++ viewer/rendering.lisp
@@ -3,7 +3,8 @@
 
 (defmethod render-bead ((bead top:bead) &key (color :gold) (radius 0.5))
   (let ((position (apply #'vec (top:get-position bead))))
-    (draw-sphere position radius color)))
+    (draw-sphere position radius color)
+    (draw-sphere-wires position radius 5 10 :black)))
 
 
 (defmethod render-chain ((chain top:chain) &key (bead-color :gold)
