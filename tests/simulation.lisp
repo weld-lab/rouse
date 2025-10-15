0630115677ccb003ce26e73cc0f5828ff33b50ca
atomicwelding <ctfweld@gmail.com>
2025-10-15 17:19:12 +0200

    testing forward, backward

--- tests/simulation.lisp
+++ tests/simulation.lisp
@@ -11,3 +11,18 @@
 				   :dt 1.0d-3))
 	 (state (sim:current-state sim)))
     (is (= (sim::state-temperature state) 1))))
+
+
+
+(test simulation-timeline
+  (let ((sim (sim:make-simulation :temperature 0 :gamma 0 :dt 0
+	      :chain (top:make-chain (:x 0)))))
+    (is (= 0 (sim:forward sim)))
+    (is (= 0 (sim:backward sim)))
+
+    (let ((state (sim::make-state
+		  :temperature 0 :gamma 0 :dt 0 :time 1
+		  :chain (top:make-chain (:x 1)))))
+      (sim:add-to-timeline sim state)
+      (is (= 1 (sim:simulation-cursor sim)))
+      (is (= 0 (sim:forward sim))))))
