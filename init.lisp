64a4780b9b6e448f61db81cc1fd00723c50e1006
atomicwelding <ctfweld@gmail.com>
2025-10-17 14:24:33 +0200

    stop-engine -> not portable, code smell

--- init.lisp
+++ init.lisp
@@ -2,14 +2,28 @@
 
 
 (defun start-engine ()
+  (setf ctrl:*engine-running* t)
   (unless ctrl:*swank-thread*
     (setf ctrl:*swank-thread*
 	  (bt:make-thread
 	   (lambda ()
 	     (swank:create-server :port 4005 :dont-close t)))))
-  (loop
-    (let ((task (ctrl:recv-from-render-channel)))
-      (funcall task))))
+  (loop while ctrl:*engine-running*
+	do (let ((task (ctrl:recv-from-render-channel)))
+	     (funcall task))))
 
+
+(defun stop-engine ()
+  (setf ctrl:*engine-running* nil)
+  (when ctrl:*swank-thread*
+    (ignore-errors
+      (dolist (conn swank::*connections*)
+        (swank::close-connection conn))
+      (when swank::*listener-socket*
+        (close swank::*listener-socket* :abort t)
+        (setf swank::*listener-socket* nil))
+      (sb-thread:destroy-thread ctrl:*swank-thread*) ; not portable
+      (setf ctrl:*swank-thread* nil)))
+  (sb-ext:exit :code 0))
 
 (start-engine)
