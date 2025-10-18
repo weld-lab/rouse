79f6e9b82be1b531e1a80c70541a1ba78142f7f3
atomicwelding <ctfweld@gmail.com>
2025-10-18 16:13:47 +0200

    some tests on copy methods

--- tests/topology.lisp
+++ tests/topology.lisp
@@ -9,11 +9,29 @@
     (is (= (length (top::chain-beads chain))
 	   2))))
 
+(test copy-bead-and-chain
+  (let* ((bead (top::make-bead :x 0.0))
+	 (copied-bead (top::copy-bead bead)))
+    (is (not (equal bead copied-bead)))
+    (is (equal (top:get-position bead)
+	       (top:get-position copied-bead)))
+    (setf (top::bead-x bead) 1.0)
+    (is (not (equal (top:get-position bead)
+		    (top:get-position copied-bead)))))
+  (let* ((chain (top:make-chain (:x 0.0)))
+	 (copied-chain (top:copy-chain chain)))
+    (is (not (equal chain copied-chain)))
+    (is (= (length (top:chain-beads chain))
+	   (length (top:chain-beads copied-chain))))
+    (setf (top::bead-x (first (top:chain-beads copied-chain))) 1.0)
+    (is (not (equal (top:chain-beads chain)
+		    (top:chain-beads copied-chain))))))
+
 
 (test chain-total-mass
-  (let ((chain (top:make-chain (:mass 3.0)
-			       (:mass 5.0))))
-    (is (= 8.0 (top:total-mass chain)))))
+      (let ((chain (top:make-chain (:mass 3.0)
+				   (:mass 5.0))))
+	(is (= 8.0 (top:total-mass chain)))))
 
 
 (test chain-center-of-mass
